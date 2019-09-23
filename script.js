1a71e7f73dc0d68ba43e6c5ab03e8f0eca558d08
David Millan <hello@davidmillan.me>
2019-09-23 23:51:36 +0100

    Completed S7.104. Variable Declarations with let and const

---
+++ script.js
@@ -0,0 +1,50 @@
+// Lecutre: let and const
+
+// // ES5
+// var name5 = 'Jane Smith';
+// var age5 = 25;
+// name5 = 'Jane Miller';
+// console.log(name5);
+
+// // ES6
+// const name6 = 'Jane Smith';
+// let age6 = 25;
+// name6 = 'Jane Miller';
+// console.log(name6); // Uncaught TypeError: Assignment to constant variable
+
+// // ES5
+// function driverLicence(passedTest) {
+
+//     if (passedTest) {
+//         var firstName = 'John';
+//         var yearOfBirth = 1990;
+//     }
+//     console.log(firstName + ' born in ' + yearofBirth + ', is now officially allowed to drive a car.');
+// }
+
+// driverLicence(true);
+
+// ES6
+// function driverLicence(passedTest) {
+
+//     console.log(firstName);
+//     let firstName;
+//     const yearOfBirth = 1990;
+
+//     if (passedTest) {
+//         firstName = 'John';
+//     }
+
+//     console.log(firstName + ' born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
+// }
+
+// driverLicence(true);
+
+
+var i = 23;
+
+for (var i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+console.log(i);
