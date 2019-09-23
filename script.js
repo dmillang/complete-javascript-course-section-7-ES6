42ca13014617b63f1a1d932755b6d9de9d0b8316
David Millan <hello@davidmillan.me>
2019-09-24 00:19:31 +0100

    Completed S7.107. Arrow Functions: Basics

--- script.js
+++ script.js
@@ -74,30 +74,55 @@
 ////////////////////////////////////
 // Lecutre: Strings
 
-let firstName = 'John';
-let lastName = 'Smith';
-const yearOfBirth = 1990;
+// let firstName = 'John';
+// let lastName = 'Smith';
+// const yearOfBirth = 1990;
 
-function calcAge(year) {
-    return 2016 - year;
-}
+// function calcAge(year) {
+//     return 2016 - year;
+// }
 
-// ES5 String
-console.log('This is ' + firstName + ' ' + lastName + '. He was born in ' + yearOfBirth + '. Today he is ' + calcAge(yearOfBirth) + ' years old.');
+// // ES5 String
+// console.log('This is ' + firstName + ' ' + lastName + '. He was born in ' + yearOfBirth + '. Today he is ' + calcAge(yearOfBirth) + ' years old.');
 
-// ES6 Template literal
-console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today he is ${calcAge(yearOfBirth)} years old.`);
+// // ES6 Template literal
+// console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today he is ${calcAge(yearOfBirth)} years old.`);
 
-const n = `${firstName} ${lastName}`;
+// const n = `${firstName} ${lastName}`;
 
-// startsWith
-console.log(n.startsWith('J'));
+// // startsWith
+// console.log(n.startsWith('J'));
 
-// endsWith
-console.log(n.endsWith('h'));
+// // endsWith
+// console.log(n.endsWith('h'));
 
-// includes
-console.log(n.includes(' '));
+// // includes
+// console.log(n.includes(' '));
 
-// repeat
-console.log(`${firstName} `.repeat(5));
+// // repeat
+// console.log(`${firstName} `.repeat(5));
+
+////////////////////////////////////
+// Lecutre: Arrow Functions
+
+const years = [1990, 1965, 1982, 1937];
+
+// ES5
+var ages5 = years.map(function(el) {
+    return 2016 - el;
+});
+
+console.log(ages5);
+
+// ES6
+let ages6 = years.map((el, i) => `Age element ${i + 1}: ${2019 - el}`);
+console.log(ages6);
+
+ages6 = years.map((el, index) => {
+    const now = new
+    Date().getFullYear();
+    const age = now - el;
+    return `Age element ${index + 1}: ${age}.`;
+});
+
+console.log(ages6)
